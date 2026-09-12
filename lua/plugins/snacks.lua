a8fdc2494c73e36caccdee15f7e71d0491d9dbde
ChenPP-123 <chenyule0429@qq.com>
2026-09-12 23:27:16 +0800

    terminal keymap change

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -122,7 +122,7 @@ return {
             desc = "File Explorer",
         },
         {
-            "<C-\\>",
+            "<C-/>",
             function()
                 Snacks.terminal()
             end,

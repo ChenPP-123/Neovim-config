8c1c4b6c78e96fa69a7c599406994aa63d19ea8c
ChenPP-123 <chenyule0429@qq.com>
2026-09-12 18:49:16 +0800

    1

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,3 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
-
-vim.keymap.set("n", "<C-p>", "<cmd>bp<CR>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<C-n>", "<cmd>bn<CR>", { desc = "Next buffer" })
